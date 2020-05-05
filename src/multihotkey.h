5ab8462671df98cde6d7cebe53d026e353d9d796
sergeantkolja <sergeantkolja@yahoo.de>
2020-05-05 08:59:19 +0000

    QT4 compatibility fix added, thx to Maik

--- src/multihotkey.h
+++ src/multihotkey.h
@@ -1,15 +1,39 @@
+#include <QtGlobal>
+#include <functional> // Qt4 needs some help, because it does not know the new Qt5 Signal/Slot extensions
 #include <QAbstractButton>
 #include <QKeySequence>
 #include <QShortcut>
 #include <QString>
 
-
-class MultiHotKey
+#if QT_VERSION < 0x050000
+class LambdaWrapper : public QObject
 {
+  Q_OBJECT
+public:
+    LambdaWrapper( std::function< void() > lambda, QObject* parent = 0)
+      : QObject(parent)
+      , m_ptr( lambda )
+    {}
+
+    ~LambdaWrapper()
+    {}
+
+public slots:
+    void call() { if( m_ptr ) m_ptr(); }
+
+private:
+  std::function< void() > m_ptr;  // must be compatible to: void QAbstractButton::animateClick( int msec=100 ), we choose: void foo(void)
+};
+#endif // QT 4 or 5
+
+
+class MultiHotKey : public QObject
+{
+  Q_OBJECT
 public:
     enum Flags { HotKeyToolTipNO=false, HotKeyToolTipOK=true };
 
-    explicit MultiHotKey( bool withTooltip = HotKeyToolTipNO );
+    explicit MultiHotKey( bool withTooltip = HotKeyToolTipNO, QObject* parent = 0 );
     ~MultiHotKey();
 
     bool bindKeySequence(const QKeySequence &KeySequence, QAbstractButton *button, const QString& Tooltip1st );  // bind this seq to this button
@@ -25,9 +49,15 @@ private:
 
     typedef QPair< QAbstractButton*, QShortcut* > Accelerator_t;
     typedef QHash< QKeySequence, Accelerator_t > Hotkeys_t;
+#if QT_VERSION < 0x050000
+    typedef QHash< QKeySequence, LambdaWrapper* > Lambdas_t;
+#endif // QT 4 or 5
     typedef QHash< QAbstractButton*, QString > ToolTips_t;
 
     Hotkeys_t  m_ButtonsAndKeys;
     ToolTips_t m_ButtonsAndTips;
+#if QT_VERSION < 0x050000
+    Lambdas_t  m_Lambdas;
+#endif // QT 4 or 5
     bool       m_withTooltips;
 };
